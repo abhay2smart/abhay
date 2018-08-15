b4984e360ef0035ac620ae54dca6ab19646cf6b5
Abhayjeet Singh <abhayjeetsingh19@gmail.com>
2018-08-15 01:49:20 -0700

    Editor's Choice(ES6)
    This app provide a gaming paltform in which the player has to guess synonyms if guesses the the currect synonyms then he gets the score. This project is created with JavaScript es6.

--- Laravel_Projects/blog_post_RRPS/resources/views/auth/veryfyemailfirst.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-@section('content')
-<div class="alert alert-success">
-  Registration is done! Check your email to activate your account.
-</div>
-@endsection
-
-@section('footer')
-@endsection
